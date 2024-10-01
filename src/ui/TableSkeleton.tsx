ecead8b797d2b52b2e67722ffbe9df906f1a6269
Bartek Z <bartekzarzyk@gmail.com>
2024-10-01 15:16:46 +0200

    improve skeleton - num of rows from store

--- src/ui/TableSkeleton.tsx
+++ src/ui/TableSkeleton.tsx
@@ -1,11 +1,12 @@
 import { Skeleton } from '../shadcn/components/ui/skeleton';
-import { PAGE_SIZE } from '../utils/constants';
+import { useRowsStore } from '../store/selectRows-store';
 
 const TableSkeleton = () => {
+  const { numberOfRows } = useRowsStore();
   return (
     <div className='flex flex-col gap-2 border p-4'>
       <Skeleton className='h-20' />
-      {Array.from({ length: PAGE_SIZE }).map((_, index) => (
+      {Array.from({ length: numberOfRows }).map((_, index) => (
         <Skeleton key={index} className='h-12' />
       ))}
     </div>
